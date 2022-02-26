911e7dac3eb3c35ad96b17bde79ec1aec374d0c2
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2022-02-26 12:25:05 -0500

    finsh

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,66 +1,180 @@
-import React from 'react' 
+import React from "react";
 
-import './header.css'  
+import "./header.css";
 
-// import {  } from "react-icons/io5";
+//icons
+import { IoCartOutline } from "react-icons/io5";
 
+import { MdOutlineDisabledByDefault } from "react-icons/md";
 
-// img 
+import { FiUser } from "react-icons/fi"; 
 
-import logo from '../../Images/logo.png'
+// img
 
-import store from '../../Images/stores.png'
-import SecNav from '../Sec-Nav/SecNav'
+import logo from "../../Images/logo.png";
 
+import store from "../../Images/stores.png";
+import SecNav from "../Sec-Nav/SecNav";
 
-
+import hodey from "../../Images/hoedy.png";
 
 const Header = () => {
-  return ( 
+  return (
+    <>
+      <hr className="fline" />
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <img className="ml-3" src={logo} alt=" logo " />
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <form className="d-sm-flex   justify-content-start">
+                <input
+                  className="  form-control me-2 bg-white "
+                  type="search"
+                  placeholder="What are you looking for ? "
+                  aria-label="Search"
+                />
+              </form>
 
-    <>  
-    <hr  className='fline' />
-    <div>
-     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-     <div className="container-fluid">
-    <img  className='ml-3' src={logo} alt=" logo " />
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent"> 
+              <img src={store} alt="store" />
 
-    <form className="d-flex  align-items-start">  
-        <input className="  form-control me-2 bg-white  " type="search" placeholder="What are you looking for ? " aria-label="Search"/>
-      </form> 
+              <div className="gray-line"> </div>
 
-        <img src={store}  alt="store"  /> 
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Languges
+                  </a>
+                </li>
 
-        <div className='gray-line' >  </div>
+                {/* <<!== dropdown Cart >>  */}
 
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Languges
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li> 
-      </ul>
-     
-    </div> 
-  </div>
-</nav>  
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link "
+                    href="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    <IoCartOutline size="1.9em" />
+                  </a>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdown"
+                  >
+                    <div className="drop   ">
+                      <div className="  cart-row ">
+                        <MdOutlineDisabledByDefault
+                          className="dropdown-item_icon "
+                          size="2em"
+                        />
+                        <img
+                          src={hodey}
+                          alt="hodey"
+                          className="dropdown-item  test "
+                        />
+                        <div className="details">
+                          <div className="f-child">
+                            <span> floral print wrap dress </span>
+                            <span> size: 42 </span>
+                          </div>
 
-    <hr  className='Sline' /> 
+                          <div className="S-child">
+                            <span> qty:1 </span>
+                            <span> $50 </span>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="  cart-row ">
+                        <MdOutlineDisabledByDefault
+                          className="dropdown-item_icon  "
+                          size="2em"
+                        />
+                        <img
+                          src={hodey}
+                          alt="hodey"
+                          className="dropdown-item test "
+                        /> 
+                           <div className="details">
+                          <div className="f-child">
+                            <span> floral print wrap dress </span>
+                            <span> size: 42 </span>
+                          </div>
 
-    </div>  
-    <SecNav/>  
+                          <div className="S-child">
+                            <span> qty:1 </span>
+                            <span> $50 </span>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="cart-row ">
+                        <MdOutlineDisabledByDefault
+                          className="dropdown-item_icon "
+                          size="2em"
+                        />
+                        <img
+                          src={hodey}
+                          alt="hodey"
+                          className="dropdown-item  test "
+                        /> 
+                           <div className="details">
+                          <div className="f-child">
+                            <span> floral print wrap dress </span>
+                            <span> size: 42 </span>
+                          </div>
+
+                          <div className="S-child">
+                            <span> qty:1 </span>
+                            <span> $50 </span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    {/* <! cart-button>>  */} 
+
+                    <div className="button-cart" >
+                      <button > show all  </button>
+                      <button >  check out </button>
+                    </div>
+                  </ul>
+                </li> 
+                
+                <FiUser color="brown"  size='2rem' />
+                
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <hr className="Sline" />
+      </div>
+      <SecNav />
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
